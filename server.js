041792eaea0a5d767b87b5b46c2c1ce7d8ae0859
kashirsaeed <kashirsaeed717@gmail.com>
2022-08-23 14:15:18 +0500

    thirty one commit

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("./db.json");
-// const middlewares = jsonServer.defaults({
-//     static: "./build",
-// });
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults({
+    static: "./build",
+});
 
-// const port = process.env.port || 3001;
-// // const port = 3001;
+const port = process.env.port || 3001;
+// const port = 3001;
   
-// server.use(middlewares);
-// server.use(
-//     jsonServer.rewriter({
-//         "/api/*":"/$1",
-//     })
-// );
+server.use(middlewares);
+server.use(
+    jsonServer.rewriter({
+        "/api/*":"/$1",
+    })
+);
 
-// server.use(router);
-// server.listen(port, () => {
-//     console.log(`Server is running on ${port}`)
-// } );
+server.use(router);
+server.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+} );
