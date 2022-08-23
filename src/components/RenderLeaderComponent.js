64a3de8b3448f43aad5e85ce6caa65d20bcf8b7a
kashirsaeed <kashirsaeed717@gmail.com>
2022-08-23 16:44:51 +0500

    forty one  commit

--- src/components/RenderLeaderComponent.js
+++ src/components/RenderLeaderComponent.js
@@ -4,14 +4,19 @@ import { Media } from "reactstrap";
 function RenderLeader({ leaderName, leaderDesignation, leaderDescription , leaderImage }) {
     return (
         <Media tag="li">
-            <Media left middle>
-                <Media object src={leaderImage} alt={leaderName} />
-            </Media>
-            <Media body className="ml-5">
-                <Media heading>{leaderName}</Media>
-                <p> {leaderDesignation} </p>
-                <p>{leaderDescription}</p>
-            </Media>
+            <div className="col-12 col-md-6" >
+                <Media left middle>
+                    <Media object src={leaderImage} alt={leaderName} />
+                </Media>
+            </div>
+            <div className="col-12 col-md" >
+                <Media body className="ml-5">
+                    <Media heading>{leaderName}</Media>
+                    <p> {leaderDesignation} </p>
+                    <p>{leaderDescription}</p>
+                </Media>
+            </div>
+
         </Media>
     )
 }
